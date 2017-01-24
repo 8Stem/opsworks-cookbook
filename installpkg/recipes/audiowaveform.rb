6285611deabe3411c6ea1c1d910bdaa7f2a8bce4
Mark Puckett <mark@8stem.com>
2017-01-23 23:11:44 -0800

    blah

--- installpkg/recipes/audiowaveform.rb
+++ installpkg/recipes/audiowaveform.rb
@@ -3,6 +3,7 @@ bash "install_audiowaveform" do
     sudo add-apt-repository ppa:chris-needham/ppa
     sudo apt-get update
     sudo apt-get install audiowaveform
+    sudo apt-get update
   EOH
 
   not_if do
