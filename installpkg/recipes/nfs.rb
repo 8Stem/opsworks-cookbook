7e3773a81690892143615e597ccea89ff0c46631
Mark Puckett <mark@8stem.com>
2017-01-25 12:02:57 -0800

    mount nfs

--- installpkg/recipes/nfs.rb
+++ installpkg/recipes/nfs.rb
@@ -1,17 +1,13 @@
-bash "mount_nfs" do
-  directory "/efs" do
-    mode 0755
-    owner 'root'
-    group 'root'
-    action :create
-  end
-    
-  code <<-EOH
-    sudo apt-get install nfs-common
-    sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-1de72bb4.efs.us-west-2.amazonaws.com:/ efs
-  EOH
+directory "/efs" do
+  mode 0755
+  owner 'root'
+  group 'root'
+  action :create
+end
 
-  not_if do
-    File.exist?("/efs")
-  end
+mount "/efs" do
+  device "fs-1de72bb4.efs.us-west-2.amazonaws.com:/"
+  fstype "nfs4"
+  options "nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2"
+  action [:mount, :enable]
 end
